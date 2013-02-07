99cd1253684ea3f83d9283fd9ac7f26e6eba9ad2
Benjie Gillam <benjie@jemjie.com>
2013-02-07 21:31:07 +0000

    Autoconversion.

---
+++ convert.sh
@@ -0,0 +1,19 @@
+cat constitution.txt | sed -E \
+  -e '1s/^/#/' \
+  -e '3s/^/##/' \
+  -e 's/^ *$//' \
+  -e 's/$/\
+/g' \
+  -e 's/  /    /g' \
+  -e 's/^( *)\(([a-z0-9]*)\)  *([^ ].*)$/\1\2. \3/' \
+  -e 's/^([0-9]*)\.  *(.*)$/\1. \2/' \
+  -e "s/[‘’]/'/g" \
+  -e 's/[“”]/"/g' \
+  > constitution.tmp
+sed -E \
+ -e 's/^( *)[a-z]*\. /\11\. /' \
+ -e 's/–/--/g' \
+ constitution.tmp > constitution.md
+pandoc --from=markdown --to=html5 --output=html/constitution.html --css=style.css ./constitution.tmp
+rm constitution.tmp
+
